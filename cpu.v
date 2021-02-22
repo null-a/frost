8f9b6a901baf983509c4609b75029983805df9a8
null-a <null-a@users.noreply.github.com>
2021-02-22 15:59:06 +0000

    Add basic instruction decoding logic.

--- cpu.v
+++ cpu.v
@@ -6,6 +6,13 @@ module cpu(input clk);
    wire pc_enable;
    wire [31:0] pc;
    wire [31:0] inst;
+   wire [6:0] opcode;
+   wire [4:0] rd;
+   wire [4:0] rs1;
+   wire [4:0] rs2;
+   wire [2:0] funct3;
+   wire [6:0] funct7;
+   wire [11:0] imm;
 
    control control(.clk(clk), .step(step), .pc_enable(pc_enable));
 
@@ -14,5 +21,10 @@ module cpu(input clk);
 
    ram ram (.clk(clk), .addr(pc[31:2]), .din(0),
             .re(1'b1), .we(1'b0), .dout(inst));
+
+   decode decode (.inst(inst), .opcode(opcode),
+                  .rd(rd), .rs1(rs1), .rs2(rs2),
+                  .funct3(funct3), .funct7(funct7),
+                  .imm(imm));
 
 endmodule // cpu
---
+++ decode.v
@@ -0,0 +1,21 @@
+`default_nettype none
+
+module decode(input [31:0] inst,
+              output [6:0] opcode,
+              output [4:0] rd,
+              output [4:0] rs1,
+              output [4:0] rs2,
+              output [2:0] funct3,
+              output [6:0] funct7,
+              output [11:0] imm);
+
+   assign opcode = inst[6:0];
+   assign rd = inst[11:7];
+   assign funct3 = inst[14:12];
+   assign rs1 = inst[19:15];
+   assign rs2 = inst[24:20];
+   assign funct7 = inst[31:25];
+
+   assign imm = inst[31:20];
+
+endmodule // decode
