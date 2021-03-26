f0374be57594525074b081572c5c8ae219ed0d20
null-a <null-a@users.noreply.github.com>
2021-03-26 11:38:10 +0000

    Add comments.

--- src/top_tb.v
+++ src/top_tb.v
@@ -57,10 +57,16 @@ module top_tb;
       txchar("\n"); txchar("d"); txchar("o"); txchar("n"); txchar("e");
       #800000;
 
+      // This is useful with programs that terminate.
+      // wait(dut.cpu.halt & dut.uart.tx_empty);
+
       $display("\n==========");
       $finish;
    end
 
+   // I'd like to drive this with `fgetc` or similar, but that doesn't
+   // work.
+   // https://electronics.stackexchange.com/questions/387423/verilog-non-blocking-file-read
    task txchar (input integer c);
       begin
          rx = 0;             #sp; // start bit
